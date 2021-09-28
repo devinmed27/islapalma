5bd878a86aa25c424457711b18c7c91eb8e215f1
Deyvi Parra <deyvi.pr@gmail.com>
2021-09-27 21:39:33 -0400

    Tabs logic

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,40 +1,146 @@
-import React from 'react'
-import SubHeader from '../components/SubHeader/SubHeader'
-import s from '../styles/pages/Gallery.module.scss'
-import Gallery from "react-photo-gallery"
-
+import React, { useState, useEffect } from "react";
+import SubHeader from "../components/SubHeader/SubHeader";
+import s from "../styles/pages/Gallery.module.scss";
+import Gallery from "react-photo-gallery";
 
 const gallery = () => {
+  const [flag, setFlag] = useState([true, false, false, false]);
+  const [indexSelected, setIndexSelected] = useState(0);
+  const photos = [
+    [
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom2.jpg",
+        width: 4,
+        height: 3,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom3.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom4.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom5.jpg",
+        width: 4,
+        height: 3,
+      },
+    ],
+    [
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom2.jpg",
+        width: 2,
+        height: 2,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom3.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom4.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom5.jpg",
+        width: 4,
+        height: 3,
+      },
+    ],
+    [
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom2.jpg",
+        width: 2,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom3.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom4.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom5.jpg",
+        width: 4,
+        height: 3,
+      },
+    ],
+    [
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom2.jpg",
+        width: 5,
+        height: 2,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom3.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom4.jpg",
+        width: 3,
+        height: 4,
+      },
+      {
+        src: "http://localhost:3000/static/assets/bannerRoom5.jpg",
+        width: 4,
+        height: 3,
+      },
+    ],
+  ];
 
-    const photos  = [
-        {
-            src : "http://localhost:3000/static/assets/bannerRoom2.jpg",
-            width : 4,
-            height: 3
-        },
-        {
-            src : "http://localhost:3000/static/assets/bannerRoom3.jpg",
-            width : 3,
-            height: 4
-        },
-        {
-            src : "http://localhost:3000/static/assets/bannerRoom4.jpg",
-            width : 3,
-            height: 4
-        },
-        {
-            src : "http://localhost:3000/static/assets/bannerRoom5.jpg",
-            width : 4,
-            height: 3
-        },
-    ]
-    return (
-        <div className={s.container}>
-            <SubHeader text='Galería' />
-            Gallery
-            <Gallery photos={photos} />
+  const handleClick = (index) => {
+    const auxArr = [false, false, false, false];
+    auxArr[index] = true;
+    setFlag(auxArr);
+  };
+
+  useEffect(() => {
+    const index = flag.indexOf(true);
+    setIndexSelected(index);
+  }, [flag]);
+
+  return (
+    <div className={s.container}>
+      <SubHeader text="Galería" />
+      <div className={s.tabsContainer}>
+        <div
+          className={flag[0] ? s.selectedTab : s.tab}
+          onClick={() => handleClick(0)}
+        >
+          
         </div>
-    )
-}
+        <div
+          className={flag[1] ? s.selectedTab : s.tab}
+          onClick={() => handleClick(1)}
+        >
+          cocina
+        </div>
+        <div
+          className={flag[2] ? s.selectedTab : s.tab}
+          onClick={() => handleClick(2)}
+        >
+          bar
+        </div>
+        <div
+          className={flag[3] ? s.selectedTab : s.tab}
+          onClick={() => handleClick(3)}
+        >
+          naturaleza
+        </div>
+      </div>
 
-export default gallery
+      <Gallery photos={photos[indexSelected]} />
+    </div>
+  );
+};
+
+export default gallery;
--- pages/location.jsx
+++ pages/location.jsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import location_1 from "../public/static/assets/location-info.jpg";
 import location_2 from "../public/static/assets/location-info2.jpg";
 
-
 const location = () => {
   const info = [
     {
@@ -14,7 +13,8 @@ const location = () => {
       subtitle: "Desde Cartagena",
       texts: [
         {
-          text: "Te recomendamos que la primera noche la tomes en Cartagena, ya que la salida a la Isla es temprano.",
+          text:
+            "Te recomendamos que la primera noche la tomes en Cartagena, ya que la salida a la Isla es temprano.",
         },
         {
           text: "Duración del trayecto: 2 horas 30 Min:",
@@ -23,30 +23,36 @@ const location = () => {
           text: "Ubicación: Muelle de la Bodeguita.",
         },
         {
-          text: "Hora: Debes presentarte a las 6:45 AM o 7:45 AM (dependiendo el dia de operacion, esa información se te brinda cuando realices la reserva con las especificaciones del servicio).",
+          text:
+            "Hora: Debes presentarte a las 6:45 AM o 7:45 AM (dependiendo el dia de operacion, esa información se te brinda cuando realices la reserva con las especificaciones del servicio).",
         },
         {
-          text: "Estos servicios se deben reservar como mínimo 72 horas antes de su llegada, ya que están sujetos a cupos limitados. Reservas: +57 304 3499318 - +57 302 3200353.",
+          text:
+            "Estos servicios se deben reservar como mínimo 72 horas antes de su llegada, ya que están sujetos a cupos limitados. Reservas: +57 304 3499318 - +57 302 3200353.",
         },
         {
           text: "Valor: $150.000 pesos por trayecto, por persona.",
         },
         {
-          text: "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores.",
+          text:
+            "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores.",
         },
       ],
-      titleSct2 : "Desde Isla Palma a Tolú",
-      textsSct2 : [
+      titleSct2: "Desde Isla Palma a Tolú",
+      textsSct2: [
         {
-          text : "1. Salida de las lanchas 3:30 PM servicios todos los días valor $55.000 por persona."
+          text:
+            "1. Salida de las lanchas 3:30 PM servicios todos los días valor $55.000 por persona.",
         },
         {
-          text : "2. Salida especial 12:30 PM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $60.000 por persona"
+          text:
+            "2. Salida especial 12:30 PM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $60.000 por persona",
         },
         {
-          text : "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores."
-        }
-      ]
+          text:
+            "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores.",
+        },
+      ],
     },
     {
       title: "Viaja por tierra hasta Tolú",
@@ -54,72 +60,71 @@ const location = () => {
       subtitle: "Desde Tolú",
       texts: [
         {
-          text: "Se opera mínimo con 3 pasajeros, se combina este servicio con un traslado terrestre carros o vans con aire acondicionado y buen servicio. Recogida en los hoteles a partir de las 5:00 AM, turistas hospedados en Bocagrande, zona norte y centro histórico) (Duración 2 horas). Traslado terrestre hasta Tolú y luego transbordo en lancha de 30 min hasta Isla Palma.",
-        }
-      ],
-      titleSct2 : "Desde Montería o Corozal a Isla Palma",
-      textsSct2 : [
-        {
-          text : "Desde tu ciudad de origen puedes tomar un vuelo nacional o internacional hasta el aeropuerto de Montería ó Corozal."
-        },
-        {
-          text : "Traslado aeropuerto - Tolú: 3126073492 - 318 2830001."
-        },
-        {
-          text : "Montería a Tolú (duración trayecto 2 horas). Corozal a Tolú (duración trayecto 1h 30 m). Ten en cuenta estos trayectos para tu conexión con la lancha. Duración del trayecto náutico 40 minutos."
-        },
-        {
-          text : "Salida de las lanchas 8:30 AM servicios todos los días valor $55.000 por persona, existen diferentes opciones de club náuticos que te pueden brindar este servicio."
-        },
-        {
-          text : "Salida especial 10:30 AM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $55.000 por persona."
-        },
-        {
-          text : " ** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores."
+          text:
+            "Se opera mínimo con 3 pasajeros, se combina este servicio con un traslado terrestre carros o vans con aire acondicionado y buen servicio. Recogida en los hoteles a partir de las 5:00 AM, turistas hospedados en Bocagrande, zona norte y centro histórico) (Duración 2 horas). Traslado terrestre hasta Tolú y luego transbordo en lancha de 30 min hasta Isla Palma.",
         },
       ],
-      titleSct3 : "Desde Isla Palma a Tolú",
-      textsSct3 : [
+      titleSct2: "Desde Montería o Corozal a Isla Palma",
+      textsSct2: [
         {
-          text : "1. Salida de las lanchas 3:30 PM servicios todos los días valor $55.000 por persona."
+          text:
+            "Desde tu ciudad de origen puedes tomar un vuelo nacional o internacional hasta el aeropuerto de Montería ó Corozal.",
         },
         {
-          text : "2. Salida especial 12:30 PM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $60.000 por persona"
+          text: "Traslado aeropuerto - Tolú: 3126073492 - 318 2830001.",
         },
         {
-          text : "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores."
-        }
-      ]
-    }
+          text:
+            "Montería a Tolú (duración trayecto 2 horas). Corozal a Tolú (duración trayecto 1h 30 m). Ten en cuenta estos trayectos para tu conexión con la lancha. Duración del trayecto náutico 40 minutos.",
+        },
+        {
+          text:
+            "Salida de las lanchas 8:30 AM servicios todos los días valor $55.000 por persona, existen diferentes opciones de club náuticos que te pueden brindar este servicio.",
+        },
+        {
+          text:
+            "Salida especial 10:30 AM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $55.000 por persona.",
+        },
+        {
+          text:
+            " ** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores.",
+        },
+      ],
+      titleSct3: "Desde Isla Palma a Tolú",
+      textsSct3: [
+        {
+          text:
+            "1. Salida de las lanchas 3:30 PM servicios todos los días valor $55.000 por persona.",
+        },
+        {
+          text:
+            "2. Salida especial 12:30 PM. Todos los días reserva con anterioridad cupos limitados comunícate 3126073492 - 318 2830001. Valor $60.000 por persona",
+        },
+        {
+          text:
+            "** Recomendación reservar con anterioridad los dos trayectos, el pago de estos servicios es directamente con los operadores.",
+        },
+      ],
+    },
   ];
-  const [flagLeft, setFlagLeft] = useState(true);
-  const [flagRight, setFlagRight] = useState(false);
-
-  const handleLeft = () => {
-    setFlagLeft(true);
-    setFlagRight(false);
-  };
-  const handleRight = () => {
-    setFlagLeft(false);
-    setFlagRight(true);
-  };
+  const [flag, setFlag] = useState(true);
 
   return (
     <div className={s.container}>
       <SubHeader text="¿Cómo llegar?" />
       <div className={s.topButtons}>
         <div className={s.left}>
-          <a className={s.title} id="left" onClick={() => handleLeft()}>
+          <a className={flag ? s.selectedTitle : s.title} onClick={() => setFlag(true)}>
             DESDE CARTAGENA
           </a>
         </div>
         <div className={s.right}>
-          <a className={s.title} id="right" onClick={() => handleRight()}>
+          <a className={flag ? s.title : s.selectedTitle} onClick={() => setFlag(false)}>
             DESDE TOLÚ
           </a>
         </div>
       </div>
-      {flagLeft && (
+      {flag ? (
         <div className={s.infoPpal}>
           <h2> {info[0].title}</h2>
           <div className={s.imgContainer}>
@@ -135,29 +140,27 @@ const location = () => {
             return <p>{element.text}</p>;
           })}
         </div>
-      )}
-
-      {flagRight && (
+      ) : (
         <div className={s.infoPpal}>
-        <h2> {info[1].title}</h2>
-        <div className={s.imgContainer}>
-          <Image src={info[1].imgPath} />
+          <h2> {info[1].title}</h2>
+          <div className={s.imgContainer}>
+            <Image src={info[1].imgPath} />
+          </div>
+          <h2 className={s.subTitle}>{info[0].subtitle}</h2>
+          {info[1].texts.map((element) => {
+            return <p>{element.text}</p>;
+          })}
+          <div className={s.line}></div>
+          <h2>{info[1].titleSct2}</h2>
+          {info[1].textsSct2.map((element) => {
+            return <p>{element.text}</p>;
+          })}
+          <div className={s.line}></div>
+          <h2>{info[1].titleSct3}</h2>
+          {info[1].textsSct3.map((element) => {
+            return <p>{element.text}</p>;
+          })}
         </div>
-        <h2 className={s.subTitle}>{info[0].subtitle}</h2>
-        {info[1].texts.map((element) => {
-          return <p>{element.text}</p>;
-        })}
-        <div className={s.line}></div>
-        <h2>{info[1].titleSct2}</h2>
-        {info[1].textsSct2.map((element) => {
-          return <p>{element.text}</p>;
-        })}
-        <div className={s.line}></div>
-        <h2>{info[1].titleSct3}</h2>
-        {info[1].textsSct3.map((element) => {
-          return <p>{element.text}</p>;
-        })}
-      </div>
       )}
     </div>
   );
