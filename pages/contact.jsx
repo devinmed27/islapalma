3e473c540b68a485f43a3eba133e9cf5bb4bfa5a
Andres Calle <andrescalleuribe@gmail.com>
2021-09-30 20:56:36 -0500

    contact card desktop view

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ContactCard from "../components/ContactCard/ContactCard";
 import s from "../styles/pages/Contact.module.scss";
 import location from "../public/static/assets/location.png";
 import wpp from "../public/static/assets/whatsapp2.png";
 import Image from "next/image";
-import banner from "../public/static/assets/contact-banner.png"
+import banner from "../public/static/assets/contact-banner.png";
+import SubHeader from "../components/SubHeader/SubHeader";
 
 const contact = () => {
+  const [size, setSize] = useState(null);
+
+  useEffect(() => {
+    setSize(screen.width);
+  }, []);
+
   return (
     <div className={s.container}>
-      <div className={s.bannerContainer}>
-        <Image src={banner} />
-      </div>
+      {size < 768 ? (
+        <div className={s.bannerContainer}>
+          <Image src={banner} />
+        </div>
+      ) : (
+        <SubHeader text="CONTÁCTANOS" />
+      )}
       <div className={s.title}>
         <h1>¡Escríbenos!</h1>
         <div></div>
@@ -23,7 +34,6 @@ const contact = () => {
           message={"Escríbenos desde cualquier lugar, y en cualquier momento"}
           phone={"+57 314 525 1610"}
           phone2={"+57 314 551 9353"}
-
         />
         <ContactCard
           img={location}
