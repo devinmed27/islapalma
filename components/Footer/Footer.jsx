ed7af8e6d6f0e8cbb718cfc5c8e2194f756d7be4
Andres Calle <andrescalleuribe@gmail.com>
2021-10-02 07:18:56 -0500

    footer desktop view

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import s from "./Footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import whatsappPath from "../../public/static/assets/whatsapp.png";
-import facebookPath from "../../public/static/assets/facebook.png";
-import instagramPath from "../../public/static/assets/instagram.png";
+import whatsappPathM from "../../public/static/assets/whatsapp.png";
+import facebookPathM from "../../public/static/assets/facebook.png";
+import instagramPathM from "../../public/static/assets/instagram.png";
 import phonePath from "../../public/static/assets/phone_icon.png";
 import emailPath from "../../public/static/assets/email_icon.png";
 import lagunaPath from "../../public/static/assets/laguna_beach.png";
 import caminoPath from "../../public/static/assets/camino_verde.png";
 import coPath from "../../public/static/assets/co_colombia.png";
 
+import whatsappPathD from "../../public/static/assets/whatsappD.png";
+import facebookPathD from "../../public/static/assets/facebookD.png";
+import instagramPathD from "../../public/static/assets/instagramD.png";
+
+import logoPath from "../../public/static/assets/logoFooter.png";
+
 const Footer = () => {
+  const [size, setSize] = useState(null);
+
+  useEffect(() => {
+    setSize(screen.width);
+  }, []);
+
   return (
     <footer className={s.container}>
       <div className={s.linksContainer}>
@@ -26,15 +38,27 @@ const Footer = () => {
         </Link>
       </div>
       <div className={s.socialContainer}>
-        <p>Siguenos en nuestras redes</p>
+        <p className={s.text}>Síguenos en nuestras redes</p>
         <div className={s.icons}>
-          <Image src={whatsappPath} />
-          <Image src={instagramPath} />
-          <Image src={facebookPath} />
+          <Link href="https://wa.link/5x4d89">
+            <a>
+              <Image src={size < 768 ? whatsappPathM : whatsappPathD} />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <Image src={size < 768 ? instagramPathM : instagramPathD} />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <Image src={size < 768 ? facebookPathM : facebookPathD} />
+            </a>
+          </Link>
         </div>
       </div>
       <div className={s.contactContainer}>
-        <p>Escríbenos o llámanos</p>
+        <p className={s.text}>Escríbenos o llámanos</p>
         <div className={s.contactInfo}>
           <Image src={phonePath} width={16} height={16} />
           <p>(+ 57) 314 525 1610 (+57) 314 551 9353</p>
@@ -45,30 +69,41 @@ const Footer = () => {
         </div>
       </div>
       <div className={s.logosContainer}>
-        <p>visitanos también en </p>
-        <div>
-          <Link href="/">
-            <a>
-              <Image src={lagunaPath} />
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <Image src={caminoPath} />
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <Image src={coPath} />
-            </a>
-          </Link>
+        {size < 768 ? (
+          <div></div>
+        ) : (
+          <div className={s.imgContainer}>
+            <Image src={logoPath} />
+          </div>
+        )}
+        <div className={s.subContainer}>
+          <p>Visitanos también en </p>
+          <div>
+            <Link href="/">
+              <a>
+                <Image src={lagunaPath} />
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <Image src={caminoPath} />
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <Image src={coPath} />
+              </a>
+            </Link>
+          </div>
         </div>
+        <p className={s.legalText}>
+          2021 Hotel Isla Palma - todos los derechos reservados. RNT No. 53813 -
+          NIT. 900.648.506-1
+        </p>
+        <p className={s.legalText}>
+          Desarrollado por <a href="https://devinmed.com">Dev in Med</a>
+        </p>
       </div>
-      <p className={s.legalText}>
-        2021 Hotel Isla Palma - todos los derechos reservados. RNT No. 53813 -
-        NIT. 900.648.506-1
-      </p>
-      <p className={s.legalText}>Desarrollado por <a href="https://devinmed.com">Dev in Med</a></p>
     </footer>
   );
 };
