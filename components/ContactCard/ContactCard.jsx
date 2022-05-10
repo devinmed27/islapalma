bd285281e4c4813c26641bfed2cbd3e4dfa8426b
Deyvi Parra <deyvi.pr@gmail.com>
2022-05-10 11:01:10 -0500

    change wa link

--- components/ContactCard/ContactCard.jsx
+++ components/ContactCard/ContactCard.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import s from "./ContactCard.module.scss";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import s from './ContactCard.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const WppCard = ({img, title, message, phone, phone2}) => {
+const WppCard = ({ img, title, message, phone, phone2 }) => {
   return (
     <div className={s.card}>
       <div className={s.imgContainer}>
-        <Image src={img} objectFit="cover" alt="logo"/>
+        <Image src={img} objectFit='cover' alt='logo' />
       </div>
-      <Link href="https://wa.link/5x4d89">
-        <a target="_blank" rel="noreferrer">
+      <Link href='https://wa.link/a8rrwf'>
+        <a target='_blank' rel='noreferrer'>
           <p className={s.title}>{title}</p>
           <p>{message}</p>
           <p className={s.phone}>{phone}</p>
@@ -18,7 +18,7 @@ const WppCard = ({img, title, message, phone, phone2}) => {
         </a>
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default WppCard;
+export default WppCard
--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,114 +1,97 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import s from "./Header.module.scss";
-import { es_lang } from "../../utils/translations/home";
-import logoPath from "../../public/static/assets/logo.png";
-import barsPath from "../../public/static/assets/menu.png";
-import Link from "next/link";
-import wppIcon from "../../public/static/assets/whatsappHeader.png";
-import igIcon from "../../public/static/assets/instagramHeader.png";
-import fbIcon from "../../public/static/assets/facebookHeader.png";
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import s from './Header.module.scss'
+import { es_lang } from '../../utils/translations/home'
+import logoPath from '../../public/static/assets/logo.png'
+import barsPath from '../../public/static/assets/menu.png'
+import Link from 'next/link'
+import wppIcon from '../../public/static/assets/whatsappHeader.png'
+import igIcon from '../../public/static/assets/instagramHeader.png'
+import fbIcon from '../../public/static/assets/facebookHeader.png'
 // import logoPath from "../../public/static/assets/logoFooter.png";
 
-
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
-  const [path, setPath] = useState("");
+  const [openMenu, setOpenMenu] = useState(false)
+  const [path, setPath] = useState('')
   const handleMenuToggle = () => {
-    setOpenMenu(!openMenu);
-  };
+    setOpenMenu(!openMenu)
+  }
 
   useEffect(() => {
-    setPath(window.location.pathname);
-  },[]);
+    setPath(window.location.pathname)
+  }, [])
 
   return (
     <header className={s.atomcontainerheader}>
       <div className={s.topContent}>
         {/* <div className={s.lang}>{`${es_lang.header.lang_selector}`} </div> */}
         <div className={s.lang}></div>
-        <Link href="/">
+        <Link href='/'>
           <a className={s.logoContainer} onClick={() => setOpenMenu(false)}>
-            <Image layout='responsive' src={logoPath} placeholder="blur" alt="logo icon"/>
+            <Image layout='responsive' src={logoPath} placeholder='blur' alt='logo icon' />
           </a>
         </Link>
         <div className={s.contact}>
           <p>CONTÁCTANOS</p>
           <div className={s.iconsContainer}>
             <div className={s.iconContainer}>
-              <a href="https://wa.link/5x4d89" target="_blank" rel="noreferrer">
-                <Image src={wppIcon} alt="whatsapp icon"/>
+              <a href='https://wa.link/a8rrwf' target='_blank' rel='noreferrer'>
+                <Image src={wppIcon} alt='whatsapp icon' />
               </a>
             </div>
             <div className={s.iconContainer}>
-              <a href="https://instagram.com/hotel_islapalma?utm_medium=copy_link" target="_blank" rel="noreferrer">
-                <Image src={igIcon} alt="whatsapp icon"/>
+              <a href='https://instagram.com/hotel_islapalma?utm_medium=copy_link' target='_blank' rel='noreferrer'>
+                <Image src={igIcon} alt='whatsapp icon' />
               </a>
             </div>
             <div className={s.iconContainer}>
-              <a href="https://www.facebook.com/isla.palma.1884" target="_blank" rel="noreferrer">
-                <Image src={fbIcon} alt="facebook icon"/>
+              <a href='https://www.facebook.com/isla.palma.1884' target='_blank' rel='noreferrer'>
+                <Image src={fbIcon} alt='facebook icon' />
               </a>
             </div>
           </div>
         </div>
         <div className={s.icon}>
-          <Image
-            onClick={handleMenuToggle}
-            className={s.barsImage}
-            src={barsPath}
-            alt="bars icon"
-          />
-          <nav
-            className={[
-              s.navMenu,
-              openMenu ? s.navMenuOpen : s.navMenuClose,
-            ].join(" ")}
-          >
-            <Link href="/">
+          <Image onClick={handleMenuToggle} className={s.barsImage} src={barsPath} alt='bars icon' />
+          <nav className={[s.navMenu, openMenu ? s.navMenuOpen : s.navMenuClose].join(' ')}>
+            <Link href='/'>
               <a className={s.logoContainer} onClick={handleMenuToggle}>
-                <Image
-                  height={90}
-                  width={120}
-                  src={logoPath}
-                  placeholder="blur"
-                  alt="logo image"
-                />
+                <Image height={90} width={120} src={logoPath} placeholder='blur' alt='logo image' />
               </a>
             </Link>
             <ul>
               <li onClick={handleMenuToggle}>
-                <Link href="/rooms">
+                <Link href='/rooms'>
                   <a>Habitaciones</a>
                 </Link>
               </li>
               <li onClick={handleMenuToggle}>
-                <Link href="/experiences">
+                <Link href='/experiences'>
                   <a>Experiencias</a>
                 </Link>
               </li>
               <li onClick={handleMenuToggle}>
-                <Link href="/promos">
+                <Link href='/promos'>
                   <a>Planes</a>
                 </Link>
               </li>
               <li onClick={handleMenuToggle}>
-                <Link href="/gallery">
+                <Link href='/gallery'>
                   <a>Galería</a>
                 </Link>
               </li>
               <li className={s.btnReserva}>
-                <a href={process.env.BOOK_URL_ES} target="_blank" rel="noreferrer">
+                <a href={process.env.BOOK_URL_ES} target='_blank' rel='noreferrer'>
                   Reserva ahora
                 </a>
               </li>
               <li onClick={handleMenuToggle}>
-                <Link href="/location">
+                <Link href='/location'>
                   <a>¿Cómo llegar?</a>
                 </Link>
               </li>
               <li onClick={handleMenuToggle}>
-                <Link href="/contact">
+                <Link href='/contact'>
                   <a>Contáctanos</a>
                 </Link>
               </li>
@@ -117,39 +100,27 @@ const Header = () => {
         </div>
       </div>
       <div className={s.bottomContent}>
-        <Link href="/rooms">
-          <a className={path.includes("rooms") ? s.selected : s.noSelected}>
-            HABITACIONES
-          </a>
+        <Link href='/rooms'>
+          <a className={path.includes('rooms') ? s.selected : s.noSelected}>HABITACIONES</a>
         </Link>
 
-        <Link href="/experiences">
-          <a
-            className={path.includes("experiences") ? s.selected : s.noSelected}
-          >
-            EXPERIENCIAS
-          </a>
+        <Link href='/experiences'>
+          <a className={path.includes('experiences') ? s.selected : s.noSelected}>EXPERIENCIAS</a>
         </Link>
 
-        <Link href="/promos">
-          <a className={path.includes("promos") ? s.selected : s.noSelected}>
-            PLANES
-          </a>
+        <Link href='/promos'>
+          <a className={path.includes('promos') ? s.selected : s.noSelected}>PLANES</a>
         </Link>
 
-        <Link href="/gallery">
-          <a className={path.includes("galery") ? s.selected : s.noSelected}>
-            GALERÍA
-          </a>
+        <Link href='/gallery'>
+          <a className={path.includes('galery') ? s.selected : s.noSelected}>GALERÍA</a>
         </Link>
-        <Link href="/location">
-          <a className={path.includes("location") ? s.selected : s.noSelected}>
-            ¿CÓMO LLEGAR?
-          </a>
+        <Link href='/location'>
+          <a className={path.includes('location') ? s.selected : s.noSelected}>¿CÓMO LLEGAR?</a>
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
