31ee005e733d9cf185a86323450a642f5b11ea65
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-06-27 10:41:43 -0500

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -68,21 +68,22 @@ const Footer = () => {
         <p className={s.text}>Escríbenos o llámanos</p>
         <div className={s.contactInfo}>
           <Image src={phonePath} width={16} height={16} alt="phine icon"/>
-           <p>(+57) 314 524 1610 </p>
-           </div>
+          <p>(+57) 314 524 1610</p>
+                 </div>
+
          <div className={s.contactInfo}>
           <Image src={phonePath} width={16} height={16} alt="phine icon"/>
-           <p> (+57) 314 551 9353</p> 
-             </div>
+          <p>(+57) 314 551 9353</p>
+                 </div>
         <div className={s.contactInfo}>
           <Image src={emailPath} width={18} height={16} alt="email icon"/>
-           <p> ventas@hotelislapalma.com </p>
-            </div>
-            <div className={s.contactInfo}>
-           <Image src={emailPath} width={18} height={16} alt="email icon"/>
-            <p> info@hotelislapalma.com</p>
-            </div>      
-      
+           <p> ventas@hotelislapalma.com</p>
+               </div>
+         <div className={s.contactInfo}>
+          <Image src={emailPath} width={18} height={16} alt="email icon"/>
+           <p> jarly@hotelislapalma.com</p>
+               </div>
+      </div>
       <div className={s.logosContainer}>
         {size < 768 ? (
           <div></div>
