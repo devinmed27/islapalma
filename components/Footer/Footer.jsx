d84484f040e811a0424e0bd53145dcc5e54ce84f
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-06-27 10:12:37 -0500

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -69,8 +69,10 @@ const Footer = () => {
         <div className={s.contactInfo}>
           <Image src={phonePath} width={16} height={16} alt="phine icon"/>
           <p>(+57) 314 524 1610<br>
-          (+57) 314 551 9353</p>
-        </div>
+            <div className={s.contactInfo}>
+          <Image src={phonePath} width={16} height={16} alt="phine icon"/>
+          <p>(+57) 314 551 9353</p>  
+                  </div>
         <div className={s.contactInfo}>
           <Image src={emailPath} width={18} height={16} alt="email icon"/>
            <p> ventas@hotelislapalma.com<br>
