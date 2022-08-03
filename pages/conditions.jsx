60604a08f338ddbea85480d811b47be008d8b427
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-03 15:37:35 -0500

    Update conditions.jsx

--- pages/conditions.jsx
+++ pages/conditions.jsx
@@ -5,17 +5,19 @@ import s from "../styles/pages/Terms.module.scss";
 const Conditions = () => {
   return (
     <div className={s.container}>
-      <SubHeader text="TÉRMINOS Y CONDICIONES" />
+      <SubHeader text="POLITICAS, TÉRMINOS Y CONDICIONES" />
       <div className={s.info}>
         <h3>TÉRMINOS Y CONDICIONES DE RESERVA</h3>
         <p>
-          Cuando se adquiere un plan de alojamiento incluye : Bebida de
-          bienvenida, alojamiento según acomodación y noches adquiridas
-          Alimentación tipo buffet desayuno(7:00-10:00), almuerzo (12:30-
-          14:00), cena (19:00-20:30). El día de ingreso es con almuerzo y día de
-          salida con desayuno. Servicios de WIFI en algunas áreas comunes del
-          hotel Visita al Bioparque, Kayaks y tablas de Paddle Board, sillas
-          asoleadoras, camas balinesas, piscina, bar.
+          Cuando se adquiere un plan de alojamiento incluye :
+Bebida de bienvenida, alojamiento según acomodación y noches adquiridas
+Alimentación tipo buffet desayuno(7:00-10:00), almuerzo (12:30- 14:00), cena (19:00-20:30). La alimentación está sujeta a la solicitud de reserva. Pensión completa; el día de ingreso es con almuerzo y el día de salida con desayuno.
+Servicios de  WIFI en algunas  áreas comunes del hotel
+Visita al Bioparque, Kayaks y tablas de Paddle Board, sillas asoleadoras, camas balinesas, piscina, bar.
+Entrega de habitaciones (CHECK-IN): a partir de las 15 horas 
+Salida de habitaciones (CHECK-OUT): hasta las 11 horas
+ Horario de Bares de 10:00 AM a 12:00 PM 
+
         </p>
         <p>
           NO INCLUYE: Transporte Cartagena o Tolú, favor revisar la información
@@ -35,54 +37,26 @@ const Conditions = () => {
           a entregar para poder organizar la limpieza y desinfección.
         </p>
         <p>
-          MENORES DE EDAD (menores de 18 años): Por disposiciones de ley, todo
-          niño o menor de edad debe presentar documento de identificación a su
-          llegada al Hotel (Registro civil o tarjeta de identidad) y estar
-          acompañado por alguno de sus padres o un adulto responsable sin
-          excepción. En caso de que el cliente no presente alguno de los
-          documentos aquí ́indicados, el Hotel se reserva la facultad de no
-          permitir el ingreso de la persona o personas del grupo que no
-          presenten los documentos completos.
+          ENTREGA DE LA HABITACIÓN (CHECK IN) / SALIDA POSTERIOR A LA HORA ESTABLECIDA (EARLY CHECK-IN / LATE CHECK OUT: Las habitaciones son entregadas hasta las 15:00, si su llegada a la Isla es temprano, le invitamos a guardar su equipaje en recepción y disfrutar de las instalaciones del hotel, mientras le entregan su habitación. Su Entrega de la Habitación o Check out es hasta las 11:00, los invitamos a entregar para poder organizar la limpieza y desinfección. 
+
         </p>
         <p>
-          POLÍTICA DE NIÑOS: Niños de 4 a 7 pagan tarifa de niño y su menú es
-          infantil, tiene una cama adicional a su reserva. Menores de 4 años no
-          tienen ningún cobro y duermen en la misma cama con el adulto
-          responsable.
+          MENORES DE EDAD (menores de 18 años): Por disposiciones de ley, todo niño o menor de edad debe presentar documento de identificación a su llegada al Hotel (Registro civil o tarjeta de identidad) y estar acompañado por alguno de sus padres o un adulto responsable sin excepción. En caso de que el cliente no presente alguno de los documentos aquí ́ indicados, el Hotel se reserva la facultad de no permitir el ingreso de la persona o personas del grupo que no presenten los documentos completos. 
+
         </p>
         <p>
-          MASCOTAS: El hotel permite en algunas ocasiones el ingreso de mascotas
-          bajo previa solicitud, solo se aceptarán razas pequeñas, con un peso
-          máximo de 5 kilogramos y que su raza no sea considerada potencialmente
-          peligrosa según el Código Nacional de Policía de Colombia. Debe
-          reportarse el ingreso de la mascota en la reserva y deben presentar el
-          carnet de vacunas, de lo contrario no se le autorizara el ingreso y
-          los cupos de mascotas son limitados. La mascota desde estar todo
-          momento con sus dueños, y en el área del restaurante estar con su
-          correa. La mascota no puede ingresar al Bioparque de Reserva Natural
-          ya que en el se encuentran animales en su estado natural como aves y
-          mamíferos que debemos cuidar. Valor $75.000 Adicional a su reserva.
+         POLÍTICA DE NIÑOS: Menores de 4 años no tienen ningún cobro y duermen en la misma cama con el adulto responsable.
         </p>
         <p>
-          COMPROMISO DEL HUÉSPED: Los huéspedes se comprometen a tener un buen
-          comportamiento durante la estadía en el inmueble, respetando los demás
-          huéspedes, así como cuidando las instalaciones del hotel, se prohíbe
-          el comportamiento que atente contra las normas de la ley salud pública
-          y la tranquilidad de las personas alrededor. Todas las situaciones de
-          incumplimiento podrán suponer una suspensión inmediata del alojamiento
-          y dará lugar a la terminación inmediata del contrato y la salida de la
-          propiedad. Recuerda que estamos en una isla agradecemos su
-          colaboración con el uso adecuado del agua y la disposición correcta de
-          la basura. Como parte de nuestra política ambiental limitamos el uso
-          de plástico de un solo uso, les invitamos a participar de esa causa.
-          Está prohibido el ingreso de neveras, alimentos, bebidas, licores; se
-          aplica cargo de descorche en caso de incumplimiento y dispositivos de
-          sonido que alteren la tranquilidad de los demás huésped.
+          MASCOTAS: El hotel permite en algunas ocasiones el ingreso de mascotas bajo previa solicitud, sólo se aceptarán razas pequeñas, con un peso máximo de 15 kilogramos y que su raza no sea considerada potencialmente peligrosa según el Código Nacional de Policía de Colombia. Debe reportarse el ingreso de la mascota en la reserva y deben presentar el carnet de vacunas, de lo contrario no se le autorizará el ingreso y los cupos de mascotas son limitados. La mascota desde estar todo momento con sus dueños, y en el área del restaurante estar con su correa. La mascota no puede ingresar al Bioparque de Reserva Natural ya que en él se encuentran animales en su estado natural como aves y mamíferos que debemos cuidar. Valor $50.000 suplementos a su reserva.
+
         </p>
         <p>
-          HORARIO DE SILENCIO: El hotel cuenta con un horario de silencio
-          comprendido desde las 12:30 pm hasta las 8 a.m. en las áreas alrededor
-          de las habitaciones.
+          COMPROMISO DEL HUÉSPED Los huéspedes se comprometen a tener un buen comportamiento durante la estadía en el inmueble, respetando los demás huéspedes, así como cuidando las instalaciones del hotel, se prohíbe el comportamiento que atente contra las normas de la ley salud pública y la tranquilidad de las personas alrededor. Todas las situaciones de incumplimiento podrán suponer una suspensión inmediata del alojamiento y dará lugar a la terminación inmediata del contrato y la salida de la propiedad. Agradecemos su colaboración con el uso adecuado del agua y la disposición correcta de la basura. Como parte de nuestra política ambiental limitamos el uso de plástico de un solo uso, les invitamos a participar de esa causa.
+Las habitaciones cuentan con cajilla de seguridad,  el hotel no se hace responsable por perdidas de objetos personales de los huésped.
+Por políticas internas del hotel no se permite el ingreso de bebidas alcohólicas y/o alimentos.  No está permitido el porte de armas dentro de las instalaciones del hotel.
+Autorizo expresamente al HOTEL ISLA PALMA RESERVA NATURAL, para que conforme a lo estipulado en la Ley 1581 de 2012, dé el manejo, tratamiento y almacenamiento de mis datos aquí suministrados, exclusivamente para fines publicitarios
+
         </p>
         <p>
           POLÍTICA DE SONIDO y TRANQUILIDAD: En las áreas comunes como
@@ -95,15 +69,23 @@ const Conditions = () => {
           $1.000.000 COP.
         </p>
         <p>
-          LLAVES PÉRDIDAS: en el momento de recibir las llaves o habitación se
-          hace responsable de la misma y en caso de pérdida se realizará un
-          cargo automático por valor de $50.000 COP.
+         HORARIO DE SILENCIO El hotel cuenta con un horario de silencio comprendido desde las 12:30 pm hasta las 8 a.m. en las áreas alrededor de las habitaciones.
         </p>
         <p>
-          NO ABUSO SEXUAL: El hotel el hotel Isla Palma está comprometido con el
-          código de conducta contra la explotación y violencia sexual infantil y
-          en cumplimiento de los artículos 16 y 17 de la ley 679.
+         POLÍTICA DE SONIDO y TRANQUILIDAD: En las áreas comunes como restaurante, bares, piscina y playa, tenemos música especial para cada ambiente a un volumen agradable para todos, le pedimos por favor se abstenga de prender parlantes.
         </p>
+         <p>
+           NO FUMAR Nuestras habitaciones y espacios abiertos son espacios libres de humo por lo tanto no está permitido fumar en ninguna zona del establecimiento la omisión tendrá un recargo que serán cargados automáticamente a su cuenta de $1.000.000 COP.
+          </p>
+        <p>
+             LLAVES PERDIDAS: en el momento de recibir las llaves o habitación se hace responsable de la misma y en caso de pérdida se realizará un cargo automático por valor de $50.000 COP 
+         </p>
+ <p>
+               NO ABUSO SEXUAL: El hotel el hotel Isla Palma está comprometido con el código de conducta contra la explotación y violencia sexual infantil y en cumplimiento de los artículos 16 y 17 de la ley 679 De la ley de infancia y adolecencia.
+                      </p>
+ <p>
+           
+           </p>
       </div>
     </div>
   );
