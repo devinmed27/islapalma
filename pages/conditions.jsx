0e9c9540f507fc0d69db4e278c155ffec6074592
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-03 16:41:14 -0500

    Update conditions.jsx

--- pages/conditions.jsx
+++ pages/conditions.jsx
@@ -7,7 +7,7 @@ const Conditions = () => {
     <div className={s.container}>
       <SubHeader text="POLITICAS, TÉRMINOS Y CONDICIONES 2" />
       <div className={s.info}>
-        <h3>TÉRMINOS Y CONDICIONES DE RESERVA</h3>
+        <center> <h3>TÉRMINOS Y CONDICIONES DE RESERVA</h3> </center>
         <p>
           Cuando se adquiere un plan de alojamiento incluye :
 Bebida de bienvenida, alojamiento según acomodación y noches adquiridas
@@ -20,7 +20,7 @@ Salida de habitaciones (CHECK-OUT): hasta las 11 horas
 
         </p>
         <p>
-         <font color="#3498DB"> NO INCLUYE</font> : Transporte Cartagena o Tolú, favor revisar la información
+         <font color="#C0392B "> NO INCLUYE</font> : Transporte Cartagena o Tolú, favor revisar la información
           de COMO LLEGAR y los operadores, reserva previa es necesaria. El hotel
           no se hace responsable por gastos adicionales generados por transporte
           marítimo fuera de los horarios estipulados. Impuesto de zarpe desde
@@ -37,29 +37,29 @@ Salida de habitaciones (CHECK-OUT): hasta las 11 horas
           a entregar para poder organizar la limpieza y desinfección.
         </p>
         <p>
-          ENTREGA DE LA HABITACIÓN (CHECK IN) / SALIDA POSTERIOR A LA HORA ESTABLECIDA (EARLY CHECK-IN / LATE CHECK OUT: Las habitaciones son entregadas hasta las 15:00, si su llegada a la Isla es temprano, le invitamos a guardar su equipaje en recepción y disfrutar de las instalaciones del hotel, mientras le entregan su habitación. Su Entrega de la Habitación o Check out es hasta las 11:00, los invitamos a entregar para poder organizar la limpieza y desinfección. 
+         <font color="#C0392B "> ENTREGA DE LA HABITACIÓN (CHECK IN) / SALIDA POSTERIOR A LA HORA ESTABLECIDA (EARLY CHECK-IN / LATE CHECK OUT</font>: Las habitaciones son entregadas hasta las 15:00, si su llegada a la Isla es temprano, le invitamos a guardar su equipaje en recepción y disfrutar de las instalaciones del hotel, mientras le entregan su habitación. Su Entrega de la Habitación o Check out es hasta las 11:00, los invitamos a entregar para poder organizar la limpieza y desinfección. 
 
         </p>
         <p>
-          MENORES DE EDAD (menores de 18 años): Por disposiciones de ley, todo niño o menor de edad debe presentar documento de identificación a su llegada al Hotel (Registro civil o tarjeta de identidad) y estar acompañado por alguno de sus padres o un adulto responsable sin excepción. En caso de que el cliente no presente alguno de los documentos aquí ́ indicados, el Hotel se reserva la facultad de no permitir el ingreso de la persona o personas del grupo que no presenten los documentos completos. 
+          <font color="#C0392B ">MENORES DE EDAD (menores de 18 años)</font>  : Por disposiciones de ley, todo niño o menor de edad debe presentar documento de identificación a su llegada al Hotel (Registro civil o tarjeta de identidad) y estar acompañado por alguno de sus padres o un adulto responsable sin excepción. En caso de que el cliente no presente alguno de los documentos aquí ́ indicados, el Hotel se reserva la facultad de no permitir el ingreso de la persona o personas del grupo que no presenten los documentos completos. 
 
         </p>
         <p>
-         POLÍTICA DE NIÑOS: Menores de 4 años no tienen ningún cobro y duermen en la misma cama con el adulto responsable.
+         <font color="#C0392B ">POLÍTICA DE NIÑOS </font>: Menores de 4 años no tienen ningún cobro y duermen en la misma cama con el adulto responsable.
         </p>
         <p>
-          MASCOTAS: El hotel permite en algunas ocasiones el ingreso de mascotas bajo previa solicitud, sólo se aceptarán razas pequeñas, con un peso máximo de 15 kilogramos y que su raza no sea considerada potencialmente peligrosa según el Código Nacional de Policía de Colombia. Debe reportarse el ingreso de la mascota en la reserva y deben presentar el carnet de vacunas, de lo contrario no se le autorizará el ingreso y los cupos de mascotas son limitados. La mascota desde estar todo momento con sus dueños, y en el área del restaurante estar con su correa. La mascota no puede ingresar al Bioparque de Reserva Natural ya que en él se encuentran animales en su estado natural como aves y mamíferos que debemos cuidar. Valor $50.000 suplementos a su reserva.
+          <font color="#C0392B ">MASCOTAS</font>: El hotel permite en algunas ocasiones el ingreso de mascotas bajo previa solicitud, sólo se aceptarán razas pequeñas, con un peso máximo de 15 kilogramos y que su raza no sea considerada potencialmente peligrosa según el Código Nacional de Policía de Colombia. Debe reportarse el ingreso de la mascota en la reserva y deben presentar el carnet de vacunas, de lo contrario no se le autorizará el ingreso y los cupos de mascotas son limitados. La mascota desde estar todo momento con sus dueños, y en el área del restaurante estar con su correa. La mascota no puede ingresar al Bioparque de Reserva Natural ya que en él se encuentran animales en su estado natural como aves y mamíferos que debemos cuidar. Valor $50.000 suplementos a su reserva.
 
         </p>
         <p>
-          COMPROMISO DEL HUÉSPED Los huéspedes se comprometen a tener un buen comportamiento durante la estadía en el inmueble, respetando los demás huéspedes, así como cuidando las instalaciones del hotel, se prohíbe el comportamiento que atente contra las normas de la ley salud pública y la tranquilidad de las personas alrededor. Todas las situaciones de incumplimiento podrán suponer una suspensión inmediata del alojamiento y dará lugar a la terminación inmediata del contrato y la salida de la propiedad. Agradecemos su colaboración con el uso adecuado del agua y la disposición correcta de la basura. Como parte de nuestra política ambiental limitamos el uso de plástico de un solo uso, les invitamos a participar de esa causa.
+          <font color="#C0392B ">COMPROMISO DEL HUÉSPED</font>: Los huéspedes se comprometen a tener un buen comportamiento durante la estadía en el inmueble, respetando los demás huéspedes, así como cuidando las instalaciones del hotel, se prohíbe el comportamiento que atente contra las normas de la ley salud pública y la tranquilidad de las personas alrededor. Todas las situaciones de incumplimiento podrán suponer una suspensión inmediata del alojamiento y dará lugar a la terminación inmediata del contrato y la salida de la propiedad. Agradecemos su colaboración con el uso adecuado del agua y la disposición correcta de la basura. Como parte de nuestra política ambiental limitamos el uso de plástico de un solo uso, les invitamos a participar de esa causa.
 Las habitaciones cuentan con cajilla de seguridad,  el hotel no se hace responsable por perdidas de objetos personales de los huésped.
 Por políticas internas del hotel no se permite el ingreso de bebidas alcohólicas y/o alimentos.  No está permitido el porte de armas dentro de las instalaciones del hotel.
 Autorizo expresamente al HOTEL ISLA PALMA RESERVA NATURAL, para que conforme a lo estipulado en la Ley 1581 de 2012, dé el manejo, tratamiento y almacenamiento de mis datos aquí suministrados, exclusivamente para fines publicitarios
 
         </p>
         <p>
-          POLÍTICA DE SONIDO y TRANQUILIDAD: En las áreas comunes como
+          <font color="#C0392B ">POLÍTICA DE SONIDO y TRANQUILIDAD</font>: En las áreas comunes como
           restaurante, bares, piscina y playa, tenemos música especial para cada
           ambiente a un volumen agradable para todos, le pedimos por favor se
           abstenga de prender parlantes. NO FUMAR: Nuestras habitaciones y
@@ -69,19 +69,19 @@ Autorizo expresamente al HOTEL ISLA PALMA RESERVA NATURAL, para que conforme a l
           $1.000.000 COP.
         </p>
         <p>
-         HORARIO DE SILENCIO El hotel cuenta con un horario de silencio comprendido desde las 12:30 pm hasta las 8 a.m. en las áreas alrededor de las habitaciones.
+         <font color="#C0392B ">HORARIO DE SILENCIO</font>: El hotel cuenta con un horario de silencio comprendido desde las 12:30 pm hasta las 8 a.m. en las áreas alrededor de las habitaciones.
         </p>
         <p>
-         POLÍTICA DE SONIDO y TRANQUILIDAD: En las áreas comunes como restaurante, bares, piscina y playa, tenemos música especial para cada ambiente a un volumen agradable para todos, le pedimos por favor se abstenga de prender parlantes.
+         <font color="#C0392B ">POLÍTICA DE SONIDO y TRANQUILIDAD</font>: En las áreas comunes como restaurante, bares, piscina y playa, tenemos música especial para cada ambiente a un volumen agradable para todos, le pedimos por favor se abstenga de prender parlantes.
         </p>
          <p>
-           NO FUMAR Nuestras habitaciones y espacios abiertos son espacios libres de humo por lo tanto no está permitido fumar en ninguna zona del establecimiento la omisión tendrá un recargo que serán cargados automáticamente a su cuenta de $1.000.000 COP.
+           <font color="#C0392B ">NO FUMAR</font>: Nuestras habitaciones y espacios abiertos son espacios libres de humo por lo tanto no está permitido fumar en ninguna zona del establecimiento la omisión tendrá un recargo que serán cargados automáticamente a su cuenta de $1.000.000 COP.
           </p>
         <p>
-             LLAVES PERDIDAS: en el momento de recibir las llaves o habitación se hace responsable de la misma y en caso de pérdida se realizará un cargo automático por valor de $50.000 COP 
+             <font color="#C0392B ">LLAVES PERDIDAS</font>: en el momento de recibir las llaves o habitación se hace responsable de la misma y en caso de pérdida se realizará un cargo automático por valor de $50.000 COP 
          </p>
  <p>
-               NO ABUSO SEXUAL: El hotel el hotel Isla Palma está comprometido con el código de conducta contra la explotación y violencia sexual infantil y en cumplimiento de los artículos 16 y 17 de la ley 679 De la ley de infancia y adolecencia.
+               <font color="#C0392B ">NO ABUSO SEXUAL</font>: El hotel el hotel Isla Palma está comprometido con el código de conducta contra la explotación y violencia sexual infantil y en cumplimiento de los artículos 16 y 17 de la ley 679 De la ley de infancia y adolecencia.
                       </p>
 
  
@@ -92,16 +92,29 @@ Autorizo expresamente al HOTEL ISLA PALMA RESERVA NATURAL, para que conforme a l
       <div className={s.info}>
         <h3>TURISMO SOSTENIBLE</h3>
         <p>
-          Cuando se adquiere un plan de alojamiento incluye :
-Bebida de bienvenida, alojamiento según acomodación y noches adquiridas
-Alimentación tipo buffet desayuno(7:00-10:00), almuerzo (12:30- 14:00), cena (19:00-20:30). La alimentación está sujeta a la solicitud de reserva. Pensión completa; el día de ingreso es con almuerzo y el día de salida con desayuno.
-Servicios de  WIFI en algunas  áreas comunes del hotel
-Visita al Bioparque, Kayaks y tablas de Paddle Board, sillas asoleadoras, camas balinesas, piscina, bar.
-Entrega de habitaciones (CHECK-IN): a partir de las 15 horas 
-Salida de habitaciones (CHECK-OUT): hasta las 11 horas
- Horario de Bares de 10:00 AM a 12:00 PM 
+          HOTEL ISLA PALMA RESERVA NATURAL  estamos comprometidos con la conservación, protección y buen manejo de los recursos naturales, así como con el fortalecimiento de la responsabilidad social y cultural de nuestras comunidades locales, mediante la sensibilización de nuestro personal, proveedores y visitantes sobre buenas prácticas ambientales.
+ 
 
         </p>
+
+<p> Comercio ilegal de vida silvestre
+HOTEL ISLA PALMA RESERVA NATURAL  está en contra del comercio ilegal de especies de flora y fauna, por lo que lo invitamos a no comprar ni consumir productos de especies en peligro de extinción. Ley 165 de 1994, Ley 1453 de 2011; Resolución 192 de 2014 (Colombia).
+
+            </p>
+
+<p> Manejo de residuos
+En el HOTEL ISLA PALMA RESERVA NATURAL  estamos comprometidos con la separación y ordenamiento de todo tipo de residuos que generamos.
+Los residuos utilizables (cartón, vidrio, plástico, latas y papel) se envían a una empresa de recicladores que se encarga de su gestión y reutilización
+Hemos reducido a un 80% el uso de plástico de un solo uso.
+Los aceites usados se entregan a la FUNDACIÓN TURISMO SOSTENIBLE, que se encarga de la producción de jabones ecológicos
+Los residuos orgánicos (cáscaras y cascarillas de frutas y verduras, restos de café y té, etc.) Se utilizan para preparar un abono orgánico con fines de jardinería en las áreas verdes de las instalaciones del hotel.
+
+            </p>
+
+<p>Energía renovable
+Contamos  con 36 paneles solares que nos proveen 60 kwa para el uso de la energía durante el día.En la noche trabajamos con planta eléctrica para minimizar las emisiones de CO2
+
+  </p>
  </div>
  </div>
     
