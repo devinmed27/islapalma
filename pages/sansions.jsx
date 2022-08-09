3a41ac258e189742b1f7ebe3235845551905f081
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 16:37:56 -0500

    Update sansions.jsx

--- pages/sansions.jsx
+++ pages/sansions.jsx
@@ -14,7 +14,7 @@ const Sansions = () => {
 
           </h3>
 
-        <p> <img src="https://www.example.com/images/dinosaur.jpg"> </p>
+        <p style="text-align: center; "> <img src="https://www.hotelcaminoverde.co/images/sostenibilidad/ojos-en-todas-partes.jpg" alt="imagen 3" width="200"></p>
           <p>
           TEMPORADA ALTA: Desde el 27 de marzo hasta el 4 de
           abril de 2021 y diciembre 21 de 2021 a Enero 16 de 2022 • 30 ó + días
