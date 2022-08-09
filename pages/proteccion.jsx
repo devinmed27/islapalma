fff990e965388d37d9cef73472ee6bc92eb553cb
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 17:41:03 -0500

    Update proteccion.jsx

--- pages/proteccion.jsx
+++ pages/proteccion.jsx
@@ -18,24 +18,39 @@ const Proteccion = () => {
 <p>Tenemos especial interés en proteger y respetar su información y datos personales y por ello hemos diseñado estas políticas de tratamiento de la información, dentro del marco de la ley 1581 de 2012 y el decreto reglamentario 1377 de 2.013.
 <br />
 <br />
-<h5>1.1.- Introducción.</h5>
+<h4>1.1 - Introducción.</h4>
 <br />
 <br />
-3. Proteger a los menores de edad nacionales o extranjeros de toda forma de explotación y violencia sexual originada por turistas nacionales o extranjeros.<br />
+Logística HOTEL ISLA PALMA RESERVA NATURAL y las sociedades adscritas, operadoras de los hoteles “una experiencia GHL” y responsables del tratamiento de la información, podrán recolectar datos personales de sus usuarios, huéspedes o visitantes, a través de los distintos medios destinados para el acceso a los servicios prestados por éstos. En todo caso, la recolección se hará bajo expresa autorización del titular de los datos y el tratamiento de estos estará sujeto a lo establecido por la ley.
 <br />
-4. Incorporar en nuestra publicidad turística, información sobre las consecuencias legales de la explotación, la pornografía y el turismo sexual con niños, niñas y adolescentes.<br />
 <br />
-7. Abstenernos de dar información a los turistas, directamente o por intermedio de sus empleados acerca de lugares desde donde se coordinen o donde se presten servicios sexuales con menores de edad.<br />
+La información personal objeto de las consideraciones acá establecidas, podrá ser recolectada directamente de la información suministrada y contenida en la tarjeta de registro hotelero, a través de la página web www.hotelislapalma.com, mediante la visita o la adquisición de servicios ofrecidos en la plataforma, o directamente en los hoteles vinculados o asociados a GHL.
 <br />
-8. Abstenernos de conducir a los turistas a establecimientos o lugares donde se practique la prostitución de menores de edad.<br />
 <br />
-9. Abstenernos de conducir a los menores de edad, directamente o por intermedio de los empleados, a los sitios donde se encuentran hospedados los turistas, incluso si se trata de lugares localizados en altamar, con fines de prostitución de menores de edad.<br />
+Se entenderán aceptadas las consideraciones acá establecidas por el Titular de la Información, cuando suscriba la Tarjea de Registro Hotelero respecto de la información allí contenida, cuando visite o haga uso de la página web www.hotelislapalma.com y/o cuando ingrese datos o información personal a través de las funciones establecidas para ello, sin importar cuál sea el fin.
 <br />
-10. Abstenernos de arrendar o utilizar vehículos en rutas turísticas con fines de prostitución o de abuso sexual con menores de edad.<br />
 <br />
-11. Impedir el ingreso de menores a los hoteles o lugares de alojamiento y hospedaje, bares, negocios similares y demás establecimientos turísticos con fines de prostitución o de abuso sexual de menores de edad.<br />
+<h4>1.2 - Principios generales.</h4><br />
 <br />
-12. Denunciar ante el Ministerio de Comercio, Industria y Turismo, la Fiscalía General de la Nación, el ICBF, y demás organismos competentes, los hechos relacionados con explotación sexual de menores de edad.</p>
+ La obtención y recolección de datos personales, al igual que el uso, tratamiento, procesamiento, intercambio, transferencia y transmisión de estos, estará siempre guiada por principios de legalidad, libertad, veracidad, transparencia, seguridad, confidencialidad, principio de acceso y de circulación restringida.<br />
+<br />
+<h4>1.3 - Definiciones legales.</h4><br /><br />
+<br />
+ De conformidad con la ley 1581 de 2012 y el decreto 1377 de 2.013, las siguientes definiciones regirán las políticas de tratamiento de la información personal.
+<br />
+<br />
+1.3.1.- Autorización: Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.
+1.3.2.- Base de Datos: Conjunto organizado de datos personales que sea objeto de Tratamiento;
+1.3.3.- Dato personal: Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables;
+1.3.4.- Encargado del Tratamiento: Por encargado del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento;
+1.3.5.- Responsable del Tratamiento: Por responsable del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el Tratamiento de los datos;
+1.3.6.- Datos sensibles: Por datos sensibles se entienden aquellos que afectan la intimidad del Titular o cuyo uso indebido puede generar su discriminación, tales como aquellos que revelen el origen racial o étnico, la orientación política, las convicciones religiosas o filosóficas, la pertenencia a sindicatos, organizaciones sociales, de derechos humanos o que promueva intereses de cualquier partido político o que garanticen los derechos y garantías de partidos políticos de oposición, así como los datos relativos a la salud, a la vida sexual, y los datos biométricos.
+1.3.7.- Dato público: Es el dato que no sea semiprivado, privado o sensible. Son considerados datos públicos, entre otros, los datos relativos al estado civil de las personas, a su profesión u oficio y a su calidad de comerciante o de servidor público. Por su naturaleza, los datos públicos pueden estar contenidos, entre otros, en registros públicos, documentos públicos, gacetas y boletines oficiales y sentencias judiciales debidamente ejecutoriadas que no estén sometidas a reserva.
+1.3.8.- Titular: Persona natural cuyos datos personales sean objeto de Tratamiento.
+1.3.9.- Transferencia: La transferencia de datos tiene lugar cuando el Responsable y/o Encargado del Tratamiento de datos personales, ubicado en Colombia, envía la información o los datos personales a un receptor, que a su vez es Responsable del Tratamiento y se encuentra dentro o fuera del país.
+1.3.10- Transmisión: Tratamiento de datos personales que implica la comunicación de los mismos dentro o fuera del territorio de la República de Colombia cuando tenga por objeto la realización de un Tratamiento por el Encargado por cuenta del Responsable.
+1.3.11.- Tratamiento: Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.
+</p>
 
         
           
