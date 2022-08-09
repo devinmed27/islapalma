1feda716c09f180188729b7ef4ecdfcfdf800487
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 17:47:50 -0500

    Update proteccion.jsx

--- pages/proteccion.jsx
+++ pages/proteccion.jsx
@@ -20,7 +20,6 @@ const Proteccion = () => {
 <br />
 <h4>1.1 - Introducción.</h4>
 <br />
-<br />
 Logística HOTEL ISLA PALMA RESERVA NATURAL y las sociedades adscritas, operadoras de los hoteles “una experiencia GHL” y responsables del tratamiento de la información, podrán recolectar datos personales de sus usuarios, huéspedes o visitantes, a través de los distintos medios destinados para el acceso a los servicios prestados por éstos. En todo caso, la recolección se hará bajo expresa autorización del titular de los datos y el tratamiento de estos estará sujeto a lo establecido por la ley.
 <br />
 <br />
@@ -31,25 +30,26 @@ Se entenderán aceptadas las consideraciones acá establecidas por el Titular de
 <br />
 <br />
 <h4>1.2 - Principios generales.</h4><br />
-<br />
+
  La obtención y recolección de datos personales, al igual que el uso, tratamiento, procesamiento, intercambio, transferencia y transmisión de estos, estará siempre guiada por principios de legalidad, libertad, veracidad, transparencia, seguridad, confidencialidad, principio de acceso y de circulación restringida.<br />
 <br />
-<h4>1.3 - Definiciones legales.</h4><br /><br />
 <br />
+<h4>1.3 - Definiciones legales.</h4><br /><br />
+
  De conformidad con la ley 1581 de 2012 y el decreto 1377 de 2.013, las siguientes definiciones regirán las políticas de tratamiento de la información personal.
 <br />
 <br />
-1.3.1.- Autorización: Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.
-1.3.2.- Base de Datos: Conjunto organizado de datos personales que sea objeto de Tratamiento;
-1.3.3.- Dato personal: Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables;
-1.3.4.- Encargado del Tratamiento: Por encargado del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento;
-1.3.5.- Responsable del Tratamiento: Por responsable del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el Tratamiento de los datos;
-1.3.6.- Datos sensibles: Por datos sensibles se entienden aquellos que afectan la intimidad del Titular o cuyo uso indebido puede generar su discriminación, tales como aquellos que revelen el origen racial o étnico, la orientación política, las convicciones religiosas o filosóficas, la pertenencia a sindicatos, organizaciones sociales, de derechos humanos o que promueva intereses de cualquier partido político o que garanticen los derechos y garantías de partidos políticos de oposición, así como los datos relativos a la salud, a la vida sexual, y los datos biométricos.
-1.3.7.- Dato público: Es el dato que no sea semiprivado, privado o sensible. Son considerados datos públicos, entre otros, los datos relativos al estado civil de las personas, a su profesión u oficio y a su calidad de comerciante o de servidor público. Por su naturaleza, los datos públicos pueden estar contenidos, entre otros, en registros públicos, documentos públicos, gacetas y boletines oficiales y sentencias judiciales debidamente ejecutoriadas que no estén sometidas a reserva.
-1.3.8.- Titular: Persona natural cuyos datos personales sean objeto de Tratamiento.
-1.3.9.- Transferencia: La transferencia de datos tiene lugar cuando el Responsable y/o Encargado del Tratamiento de datos personales, ubicado en Colombia, envía la información o los datos personales a un receptor, que a su vez es Responsable del Tratamiento y se encuentra dentro o fuera del país.
-1.3.10- Transmisión: Tratamiento de datos personales que implica la comunicación de los mismos dentro o fuera del territorio de la República de Colombia cuando tenga por objeto la realización de un Tratamiento por el Encargado por cuenta del Responsable.
-1.3.11.- Tratamiento: Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.
+1.3.1.- Autorización: Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.<br />
+1.3.2.- Base de Datos: Conjunto organizado de datos personales que sea objeto de Tratamiento;<br />
+1.3.3.- Dato personal: Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables;<br />
+1.3.4.- Encargado del Tratamiento: Por encargado del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento;<br />
+1.3.5.- Responsable del Tratamiento: Por responsable del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el Tratamiento de los datos;<br />
+1.3.6.- Datos sensibles: Por datos sensibles se entienden aquellos que afectan la intimidad del Titular o cuyo uso indebido puede generar su discriminación, tales como aquellos que revelen el origen racial o étnico, la orientación política, las convicciones religiosas o filosóficas, la pertenencia a sindicatos, organizaciones sociales, de derechos humanos o que promueva intereses de cualquier partido político o que garanticen los derechos y garantías de partidos políticos de oposición, así como los datos relativos a la salud, a la vida sexual, y los datos biométricos.<br />
+1.3.7.- Dato público: Es el dato que no sea semiprivado, privado o sensible. Son considerados datos públicos, entre otros, los datos relativos al estado civil de las personas, a su profesión u oficio y a su calidad de comerciante o de servidor público. Por su naturaleza, los datos públicos pueden estar contenidos, entre otros, en registros públicos, documentos públicos, gacetas y boletines oficiales y sentencias judiciales debidamente ejecutoriadas que no estén sometidas a reserva.<br />
+1.3.8.- Titular: Persona natural cuyos datos personales sean objeto de Tratamiento.<br />
+1.3.9.- Transferencia: La transferencia de datos tiene lugar cuando el Responsable y/o Encargado del Tratamiento de datos personales, ubicado en Colombia, envía la información o los datos personales a un receptor, que a su vez es Responsable del Tratamiento y se encuentra dentro o fuera del país.<br />
+1.3.10- Transmisión: Tratamiento de datos personales que implica la comunicación de los mismos dentro o fuera del territorio de la República de Colombia cuando tenga por objeto la realización de un Tratamiento por el Encargado por cuenta del Responsable.<br />
+1.3.11.- Tratamiento: Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.<br /><br />
 </p>
 
         
