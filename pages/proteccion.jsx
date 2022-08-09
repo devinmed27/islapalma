9dd8f00591aedfe4a272e8ce56ffb49035ac7e93
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 18:32:14 -0500

    Update proteccion.jsx

--- pages/proteccion.jsx
+++ pages/proteccion.jsx
@@ -52,7 +52,37 @@ Se entenderán aceptadas las consideraciones acá establecidas por el Titular de
 <font color="#C0392B ">1.3.11.</font> <strong>- Tratamiento:</strong> Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.<br /><br />
 </p>
 
+<h3>SEGUNDO: AUTORIZACIÓN DEL TITULAR</h3>
         
+<p>Los datos suministrados estarán sujetos a tratamiento autorizado, concedido de manera previa, expresa e informada por el Titular de los mismos. Al suministrar sus datos al ingreso del hotel para diligenciar la Tarjeta de Registro Hotelero, está autorizando el tratamiento en los términos y condiciones establecidos en la presente política.
+<br />
+En todo caso, la recolección de datos estará limitada a aquellos datos personales que sean pertinentes y adecuados para la finalidad perseguida.
+
+</p>
+
+
+<h3>TERCERO: TRATAMIENTO DE LA INFORMACIÓN</h3>
+
+<p><font color="#C0392B ">3.1</font><strong>- Datos recolectados:</strong> La recolección de datos para el desarrollo del Tratamiento y fines por éste perseguidos, recaerá sobre los datos personales que se reciban y almacenen con ocasión del diligenciamiento de la Tarjeta de Registro Hotelero, de la información que nos suministre nuestros huéspedes, clientes, visitantes y comprenderá toda la información que se suministre o proporcione en la visita a la página web www.hotelislapalma.com, así como toda aquella relacionada con los servicios o reservas efectuadas, y los datos de alojamiento y hospedaje suministrados física o virtualmente.
+<br />
+Sin perjuicio de que en algunos casos se trate de datos públicos, la información recogida y objeto de tratamiento será la correspondiente al nombre, número de documento de identificación sea cédula de ciudadanía, pasaporte o cualquier otro válido, profesión, nacionalidad, fecha de nacimiento, dirección de correo electrónico, preferencias e intereses personales, trabajo o actividad, hábitos de consumo o hábitos de viaje. Si se hace una reserva a través de la página web www.hotelislapalma.com, se recogerá la información correspondiente a la tarjeta de crédito que se suministre para los efectos de la reserva y estadía.
+<br />
+ 
+ <font color="#C0392B ">3.2</font><strong>- Tratamiento al que serán sometidos los datos y finalidad del mismo.</strong>
+   <br />
+   Los datos y la información obtenidos serán usados en el curso normal de sus actividades comerciales únicamente con la finalidad establecida en las presentes políticas de tratamiento de la información, de manera que se permita crear una comunicación directa y efectiva con el huésped, cliente o usuario, que conduzca a establecer un vínculo más cercano y a consolidar una relación comercial.
+  <br />
+     
+     El tratamiento consiste en el envío de información digital a través de diferentes vías de comunicación, con la intención de contactar con el titular para enviarle encuestas de servicios luego de cada estadía que permitan la calificación del servicio prestado, y comunicarle las invitaciones, ofertas, promociones, portafolio de servicios o información del Hotel o de los hoteles “una experiencia GHL”, sin que en momento alguno sus datos sean facilitados, cedidos o entregados a personas distintas o ajenas a los responsables o encargados del tratamiento de la información. Adicionalmente, mediante la recolección de datos se busca: realizar, procesar, tramitar y/o completar reservas o compra de noches hoteleras u otros servicios; realizar estudios internos sobre hábitos de turismo; evaluar la calidad de nuestros servicios; enviar encuestas y cuestionarios respecto de los servicios prestados; atender oportunamente sus solicitudes, peticiones o necesidades; comunicar invitaciones, ofertas, promociones, e información en general, sobre el portafolio de servicios ofrecidos por personas naturales o jurídicas que se vinculan directamente con la operación hotelera y específicamente con los servicios prestados.
+ <br />
+ La autorización para el uso de la información o datos suministrados que sean recolectados, recogidos o almacenados acorde con las presentes políticas comprende expresamente la autorización para que los datos y la información sean compartidos, procesados, transmitidos, transferidos, actualizados y/o suprimidos con el propósito definido en las presente políticas, para ser utilizados de la manera establecida.
+Al ingresar a la página web www.hotelislapalma.com autoriza que su información y datos sea compartida con los prestadores turísticos a los que se refiere y ante quienes se tramiten sus reservas y/o solicitudes.
+<br />
+  Se asume que toda la información o datos que se suministran o depositan en la Tarjeta de Registro Hotelero o a través de la página www.hotelislapalma.com es verdadera, precisa y completa, y podrá ser retirada en cualquier momento en el evento en el que se considere nociva o perjudicial para sus intereses o los intereses de un tercero.
+
+<br />
+Los datos y en general la información que se recibe cuando ingresa a la página web www.hotelislapalma.com puede ser tanto suya como del equipo desde el cual está ingresando. Con el ánimo de optimizar y hacer más eficiente su experiencia visitando la página www.hotelislapalma.com, podrán utilizarse cookies y/o web beacons, así como podrá obtenerse y almacenarse la información de las páginas de internet visitadas, de su dirección IP, del sistema operativo del equipo desde el cual esté ingresando, mediante un proceso de reconocimiento y rastreo que permita identificar sus preferencias e identificarlo cuando visite nuevamente página y almacenar determinados registros, a partir de su dirección IP. La dirección IP no se asocia ni se vincula a su nombre ni a sus datos personales.
+<br />
           
 
       </div>
