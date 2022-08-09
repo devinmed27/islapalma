0089fa4bee4ec08c061a9130e633758c3ddcb66e
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 15:01:22 -0500

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -120,11 +120,14 @@ const Footer = () => {
           Desarrollado por <a href="https://devinmed.com" target="_blank" rel="noreferrer">Dev in Med</a>
         </p>
 
+<div className={s.contactInfo}>
 <ul>
 <li><strong style="color: #00842b;">HOSPEDAJE</strong>: 3214933631 – 3154271409</li>
 <li><strong style="color: #00842b;">DAY TOUR</strong>: 3106310928– 3008169666</li>
 <li><strong style="color: #00842b;">EVENTS</strong>: &nbsp;3104605052 – 3144540777</li>
 </ul>
+</div>
+
         <div className={s.logosSubContainer}>
             <Link href="/">
               <a>
