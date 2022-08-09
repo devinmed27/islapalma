4276bdaca016e43f8e5dfb49ea4b06c3d25690cf
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-09 17:14:38 -0500

    Create proteccion.jsx

---
+++ pages/proteccion.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import SubHeader from "../components/SubHeader/SubHeader";
+import s from "../styles/pages/Terms.module.scss";
+
+
+
+const Sansions = () => {
+  return (
+    <div className={s.container}>
+      <SubHeader text="CODIGO DE CONDUCTA" />
+      <div className={s.info}>
+        <center><h3>
+          CODIGO DE CONDUCTA 
+
+          </h3></center>
+
+<p>Con el fijjjjjjstar la explotación, la pornografía y el turismo sexual con niños, niñas y adolescentes, el <strong>HOTEL ISLA PALMA</strong>&nbsp;en cumplimiento de la Ley 679 de 2001 adicionada por la Ley 1336 de 2009, se compromete a cumplir con las siguientes obligaciones legales:</p>
+
+<p>1. Abstenernos de ofrecer en los programas de promoción turística y en planes turísticos, expresa o subrepticiamente, planes de explotación sexual de menores.<br />
+<br />
+2. Adoptar las medidas tendientes a impedir que los trabajadores de <strong>HOTEL ISLA PALMA</strong>, dependientes o intermediarios, ofrezcan servicios turísticos que incluyan contactos sexuales con menores de edad.<br />
+<br />
+3. Proteger a los menores de edad nacionales o extranjeros de toda forma de explotación y violencia sexual originada por turistas nacionales o extranjeros.<br />
+<br />
+4. Incorporar en nuestra publicidad turística, información sobre las consecuencias legales de la explotación, la pornografía y el turismo sexual con niños, niñas y adolescentes.<br />
+<br />
+7. Abstenernos de dar información a los turistas, directamente o por intermedio de sus empleados acerca de lugares desde donde se coordinen o donde se presten servicios sexuales con menores de edad.<br />
+<br />
+8. Abstenernos de conducir a los turistas a establecimientos o lugares donde se practique la prostitución de menores de edad.<br />
+<br />
+9. Abstenernos de conducir a los menores de edad, directamente o por intermedio de los empleados, a los sitios donde se encuentran hospedados los turistas, incluso si se trata de lugares localizados en altamar, con fines de prostitución de menores de edad.<br />
+<br />
+10. Abstenernos de arrendar o utilizar vehículos en rutas turísticas con fines de prostitución o de abuso sexual con menores de edad.<br />
+<br />
+11. Impedir el ingreso de menores a los hoteles o lugares de alojamiento y hospedaje, bares, negocios similares y demás establecimientos turísticos con fines de prostitución o de abuso sexual de menores de edad.<br />
+<br />
+12. Denunciar ante el Ministerio de Comercio, Industria y Turismo, la Fiscalía General de la Nación, el ICBF, y demás organismos competentes, los hechos relacionados con explotación sexual de menores de edad.</p>
+
+        
+          
+
+      </div>
+    </div>
+  );
+};
+
+export default proteccion;
