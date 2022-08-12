fa0948ae86a91e5ea530f800c9a9a68683e87b8b
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-12 17:06:35 -0500

    Update proteccion.jsx

--- pages/proteccion.jsx
+++ pages/proteccion.jsx
@@ -446,62 +446,42 @@ return (
 
             <br />
             <br />
-                    
-            <h3> PRIMERO: GENERALIDADES </h3>
-</p>
 
-                <p>
-                En desarrollo de nuestras relaciones comerciales, tenemos especial interés en proteger y respetar su información y datos personales y por ello hemos diseñando estas políticas de tratamiento de la información, dentro del marco de la ley 1581 de 2012 y el decreto reglamentario 1377 de 2.013.
+        <h3> PRIMERO: GENERALIDADES </h3>
+        </p>
 
-                 <br />
-                 <br />
-                    <h4>1.1 - Introducción.</h4>
-<br />
-                 <br />
- Logística HOTEL ISLA PALMA RESERVA NATURAL y las sociedades adscritas, operadoras de los hoteles una experiencia GHL y responsables del tratamiento de la información, podrán recolectar datos personales de sus proveedores, y en general de quien les venda bienes o servicios, en desarrollo de la relación comercial para facilitar su ejecución y procurar la mayor eficiencia posible. La recolección se hará bajo expresa autorización del titular de los datos y el tratamiento de estos estará sujeto a lo establecido por la ley y la presente política.
-     <br />
-                 <br />
-     La información personal podrá ser recolectada directamente de los proveedores o a partir de aquella información contenida en el certificado de existencia y representación legal expedido por la Cámara de Comercio, en el RUT, en contratos, cotizaciones, propuestas, facturas, órdenes de compra y en general en cualquier documento que corresponda a la relación comercial.
+        <p>
+            En desarrollo de nuestras relaciones comerciales, tenemos especial interés en proteger y respetar su
+            información y datos personales y por ello hemos diseñando estas políticas de tratamiento de la información,
+            dentro del marco de la ley 1581 de 2012 y el decreto reglamentario 1377 de 2.013.
+
+            <br />
+            <br />
+        <h4>1.1 - Introducción.</h4>
+        <br />
+        <br />
+        Logística HOTEL ISLA PALMA RESERVA NATURAL y las sociedades adscritas, operadoras de los hoteles una experiencia
+        GHL y responsables del tratamiento de la información, podrán recolectar datos personales de sus proveedores, y
+        en general de quien les venda bienes o servicios, en desarrollo de la relación comercial para facilitar su
+        ejecución y procurar la mayor eficiencia posible. La recolección se hará bajo expresa autorización del titular
+        de los datos y el tratamiento de estos estará sujeto a lo establecido por la ley y la presente política.
+        <br />
+        <br />
+        La información personal podrá ser recolectada directamente de los proveedores o a partir de aquella información
+        contenida en el certificado de existencia y representación legal expedido por la Cámara de Comercio, en el RUT,
+        en contratos, cotizaciones, propuestas, facturas, órdenes de compra y en general en cualquier documento que
+        corresponda a la relación comercial.
 
 
-     <br />
-                 <br />
-         Se entenderá que el Titular de la Información ha aceptado los términos y condiciones de estas políticas cuando emita la propuesta o cotización y/o cuando suscriba el contrato correspondiente o documento equivalente.
-
-     <br />
-                 <br />
-             <font color="#C0392B ">1.2</font><strong> -Principios generales.</strong>
-     <br />
-                 <br />
-                 La obtención y recolección de datos personales, al igual que el uso, tratamiento, procesamiento, intercambio, transferencia y transmisión de estos, estará siempre guiada por principios de legalidad, libertad, veracidad, transparencia, seguridad, confidencialidad, principio de acceso y de circulación restringida.
-<br />
-                 <br />
-                     <font color="#C0392B ">1.3</font><strong> -Definiciones legales.</strong>
-                     <br />
-                 <br />
-                        De conformidad con la ley 1581 de 2012 y el decreto 1377 de 2.013, las siguientes definiciones regirán las políticas de tratamiento de la información personal.
-                     <br />
-                 <br />
-<font color="#C0392B ">1.3.1.</font> <strong>- Autorización:</strong> Consentimiento previo, expreso e informado del Titular para llevar a cabo el Tratamiento de datos personales.><br />
-<font color="#C0392B ">1.3.2.</font> <strong>- Base de Datos:</strong> Conjunto organizado de datos personales que sea objeto de Tratamiento;<br />
-<font color="#C0392B ">1.3.3.</font> <strong>- Dato personal:</strong> Cualquier información vinculada o que pueda asociarse a una o varias personas naturales determinadas o determinables;<br />
-<font color="#C0392B ">1.3.4.</font> <strong>- Encargado del Tratamiento: </strong>Por encargado del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, realice el Tratamiento de datos personales por cuenta del Responsable del Tratamiento;<br />
-<font color="#C0392B ">1.3.5.</font> <strong>- Responsable del Tratamiento: </strong>Por responsable del tratamiento se tiene a la persona natural o jurídica, pública o privada, que por sí misma o en asocio con otros, decida sobre la base de datos y/o el Tratamiento de los datos;<br />
-<font color="#C0392B ">1.3.6.</font> <strong>- Datos sensibles:</strong> Por datos sensibles se entienden aquellos que afectan la intimidad del Titular o cuyo uso indebido puede generar su discriminación, tales como aquellos que revelen el origen racial o étnico, la orientación política, las convicciones religiosas o filosóficas, la pertenencia a sindicatos, organizaciones sociales, de derechos humanos o que promueva intereses de cualquier partido político o que garanticen los derechos y garantías de partidos políticos de oposición, así como los datos relativos a la salud, a la vida sexual, y los datos biométricos.<br />
-<font color="#C0392B ">1.3.7.</font> <strong>- Dato público:</strong> Es el dato que no sea semiprivado, privado o sensible. Son considerados datos públicos, entre otros, los datos relativos al estado civil de las personas, a su profesión u oficio y a su calidad de comerciante o de servidor público. Por su naturaleza, los datos públicos pueden estar contenidos, entre otros, en registros públicos, documentos públicos, gacetas y boletines oficiales y sentencias judiciales debidamente ejecutoriadas que no estén sometidas a reserva.<br />
-<font color="#C0392B ">1.3.8.</font> <strong>- Titular:</strong> Persona natural cuyos datos personales sean objeto de Tratamiento.<br />
-<font color="#C0392B ">1.3.9.</font> <strong>- Transferencia:</strong> La transferencia de datos tiene lugar cuando el Responsable y/o Encargado del Tratamiento de datos personales, ubicado en Colombia, envía la información o los datos personales a un receptor, que a su vez es Responsable del Tratamiento y se encuentra dentro o fuera del país.<br />
-<font color="#C0392B ">1.3.10</font> <strong>- Transmisión:</strong> Tratamiento de datos personales que implica la comunicación de los mismos dentro o fuera del territorio de la República de Colombia cuando tenga por objeto la realización de un Tratamiento por el Encargado por cuenta del Responsable.<br />
-<font color="#C0392B ">1.3.11.</font> <strong>- Tratamiento:</strong> Cualquier operación o conjunto de operaciones sobre datos personales, tales como la recolección, almacenamiento, uso, circulación o supresión.
-
-                     <br />
-                 <br />
-     </p>
+        <br />
+        <br />
+        
+        </p>
 
 
 
 
-    
+
 
 
     </div>
