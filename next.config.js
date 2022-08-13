cf60f08193df75a2e9d524fa8d4958cfb3b909ef
Jarly Miguel Roman Sierra <41272579+jarlyroman@users.noreply.github.com>
2022-08-13 12:51:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    BOOK_URL_ES: "https://booking.roomcloud.net/be/se1/hotel.jsp?hotel=10810&curr=COP&lang=es&d=3",
-    BOOK_URL_EN: "https://booking.roomcloud.net/be/se1/hotel.jsp?hotel=10810&curr=USD&lang=en&d=3",
+    BOOK_URL_ES: "https://booking.roomcloud.net/be/se2/hotel.jsp?hotel=10810&curr=COP&lang=es&d=3",
+    BOOK_URL_EN: "https://booking.roomcloud.net/be/se2/hotel.jsp?hotel=10810&curr=USD&lang=en&d=3",
     URL : "https://islapalma.vercel.app/",
     CONTENTFUL_SPACE: "8guzw5cggc2y",
     CONTENTFUL_ACCESS_TOKEN: "NySvdbsA-t_x98TzzKF1DCX83mLBhqNW2L2MCOYNHU0"
